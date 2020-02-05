a875b0583c3e9554fffb475604a47c7918ca6e64
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-06 00:47:24 +0900

    add route settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page1Component },
+  { path: 'page3', component: Page1Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
